547bdd775cfe8e87ad799e888858796969c98dd9
Venelin Venkov <112377436+vvvenkov@users.noreply.github.com>
2025-12-09 15:22:43 +0100

    setup react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,73 @@
 function App() {
 	return (
 		<>
-	<h1>I love Games</h1>
+			<header>
+				{/* <!-- Navigation --> */}
+				<nav>
+					<a className="home" href="#"> <img src="./images/logo.png" alt="logo" /> </a>
+					<a href="#">Catalog</a>
+					{/* <!-- Logged-in users --> */}
+					<div id="user">
+						<a href="#">Add Game</a>
+						<a href="#">Logout</a>
+					</div>
+					{/* <!-- Guest users --> */}
+					<div id="guest">
+						<a href="#">Login</a>
+						<a href="#">Register</a>
+					</div>
+				</nav>
+			</header>
+
+			{/* <!--Home Page--> */}
+			<section id="welcome-world">
+
+				<div className="welcome-message">
+					<h2>ALL new games are</h2>
+					<h3>Only in </h3>
+					<img id="logo-left" src="./images/logo.png" alt="logo" />
+				</div>
+
+				<div id="home-page" />
+				<h1>Latest Games</h1>
+				<div id="latest-wrap">
+					{/* <!-- Display div: with information about every game (if any) --> */}
+					<div className="home-container">
+						<div className="game">
+							<img src="./images/witcher.png" alt="Elden Ring" />
+							<div className="details-overlay">
+								<p className="name">The Witcher 3</p>
+								<p className="genre">Open World</p>
+								<button className="details-button">Details</button>
+							</div>
+						</div>
+						<div className="game">
+							<img src="./images/elden ring.png" alt="Elden Ring" />
+							<div className="details-overlay">
+								<p className="name">Elden Ring</p>
+								<p className="genre">Action RPG</p>
+								<button className="details-button">Details</button>
+							</div>
+						</div>
+						<div className="game">
+							<img src="./images/minecraft.png" alt="Minecraft" />
+							<div className="details-overlay">
+								<p className="name">Minecraft</p>
+								<p className="genre">Sandbox</p>
+								<button className="details-button">Details</button>
+							</div>
+							{/* <!-- Display paragraph: If there is no games  --> */}
+							{/* <!-- <p className="no-articles">No games yet</p> --> */}
+						</div>
+
+					</div>
+
+				</div>
+				<footer>
+					<p className="center-text">React Course Exercise @ SoftUni &trade;</p>
+				</footer>
+			</section>
 		</>
 	)
 }
-
 export default App
